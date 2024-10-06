74e033eaca72e532be3eaee30d7e44f90ed7c176
Mathieu Guitter <guittermathieu@gmail.com>
2024-10-07 01:02:43 +0200

    [ADD] TextMoney

--- LudumDare/Assets/Script/MoneyManager.cs
+++ LudumDare/Assets/Script/MoneyManager.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class MoneyManager : MonoBehaviour
 {
     public int money;
+    [SerializeField] private GameObject moneyText;
+
+    void Start()
+    {
+        ChangeMoneyText();
+    }
+
+    void ChangeMoneyText()
+    {
+        moneyText.GetComponent<TMPro.TextMeshProUGUI>().text = money.ToString();
+    }
 
     public void AddMoney(int amount)
     {
         money += amount;
+        ChangeMoneyText();
     }
 
     public void SubtractMoney(int amount)
     {
         money -= amount;
+        ChangeMoneyText();
     }
 }

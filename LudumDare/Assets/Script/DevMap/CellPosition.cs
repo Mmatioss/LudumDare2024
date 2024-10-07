bfd04b1814fd39fd1dc9ff2ccb5359b90498f662
Arthur "Malukker" Beauquesne <arthur.beauquesne@gmail.com>
2024-10-07 14:43:01 +0200

    Comitting current changes to the tile system

--- LudumDare/Assets/Script/DevMap/CellPosition.cs
+++ LudumDare/Assets/Script/DevMap/CellPosition.cs
@@ -10,22 +10,22 @@ public class CellPosition : MonoBehaviour
 public Vector3Int PositionCell(Tilemap tilemap)
 {
     Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    // Debug.Log("Position de la souris: "+mouseWorldPos);
+    //Debug.Log("Position de la souris: "+mouseWorldPos);
     Vector3Int cellPosition = tilemap.WorldToCell(mouseWorldPos);
     
     cellPosition.z = 0;
     cellPosition.y += 1;
-    // Debug.Log("Position de la cellule: " + cellPosition);
+    //Debug.Log("Position de la cellule: " + cellPosition);
     return cellPosition;
 }
 
 public Vector3Int PositionCell2(Tilemap tilemap)
 {
     Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    // Debug.Log("Position de la souris: "+mouseWorldPos);
+    //Debug.Log("Position de la souris: "+mouseWorldPos);
     Vector3Int cellPosition = tilemap.WorldToCell(mouseWorldPos);
     cellPosition.z = 0;
-    // Debug.Log("Position de la cellule: " + cellPosition);
+    //Debug.Log("Position de la cellule: " + cellPosition);
     return cellPosition;
 }
 
